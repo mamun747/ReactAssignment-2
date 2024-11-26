e9236e6f1559c1f2918bf379a7b25aeb88567cff
mamun747 <technoultra42@gmail.com>
2024-11-26 21:45:43 +0600

    training section problem fixed

--- wizia/src/Components/Training.jsx
+++ wizia/src/Components/Training.jsx
@@ -10,13 +10,13 @@ function Training() {
       ];
   return (
     <>
-    <div className="bg-bg2 py-[32px] lg:py-[144px] mx-[48px] rounded-3xl flex flex-col px-[14px] md:px-0 md:items-center">
-        <div>
+    <div className="bg-bg2 py-[32px] lg:py-[144px] mx-6 md:mx-8 lg:mx-[48px] rounded-3xl flex flex-col px-[14px] md:px-0 md:items-center relative">
+        <div className="z-20">
             <h1 className="font-[600] font-Montserrat text-white text-[28px] md:text-[40px]">Train your aiDR on your...</h1>
             <h4 className="bg-btnColor bg-clip-text text-transparent italic font-[500] text-[28px] md:text-[40px] font-Montserrat pb-6 md:pb-[30px]">prefered email st|</h4>
             <p className="text-gray-400 text-lg md:text-2xl max-w-md">You’re in control. Train your aiDR on elements of your Marketing strategy.</p>
         </div>
-        <div className="grid grid-cols-1  lg:grid-cols-2 text-teal-300 pt-[30px] lg:pt-[64px]">
+        <div className="grid grid-cols-1  lg:grid-cols-2 text-teal-300 pt-[30px] lg:pt-[64px] z-10">
       {/* Left Column */}
       <div className="flex flex-col lg:gap-4">
         {featuresLeft.map((feature, index) => (
@@ -37,6 +37,8 @@ function Training() {
         ))}
       </div>
     </div>
+
+    <img src="/image4.png" className="absolute right-0 md:left-0 bg-bg2 bottom-0" alt="" />
     </div>
     </>
   )
